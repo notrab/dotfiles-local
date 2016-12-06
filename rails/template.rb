1e8832c5c64b43b131101b09f83577e42ca91e0f
Jamie Barton <jamie@launchmade.com>
2016-12-06 21:28:08 +0000

    Updated rails/template.rb

--- rails/template.rb
+++ rails/template.rb
@@ -1,44 +1,56 @@
 project_name = File.basename(__dir__)
 
-run "rm README.md"
+uncomment_lines 'Gemfile', /gem 'redis'/
 
 gem 'sidekiq'
 gem 'haml-rails'
 
-uncomment_lines 'Gemfile', /gem 'redis'/
-
 after_bundle do
-  # generate(:controller, "pages", "home")
-  route %Q(root to: "pages#home")
-
   initializer "sidekiq.rb", <<-EOF
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.secrets.redis_url, namespace: "sidekiq_#{project_name}"}
+  config.redis = { url: ENV["REDIS_URL"], namespace: "sidekiq"}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.secrets.redis_url, namespace: "sidekiq_#{project_name}"}
+  config.redis = { url: ENV["REDIS_URL"], namespace: "sidekiq"}
 end
-  EOF
+EOF
 
-  application do
-    <<-EOF
+  application do <<-EOF
 config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
     end
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
     config.active_job.queue_adapter = :sidekiq
-  EOF
+EOF
   end
 
   file "Procfile", <<-EOF
 web: bundle exec puma -C config/puma.rb
 worker: bundle exec sidekiq -q default -q mailers
-  EOF
+EOF
+
+  file "Procfile.local", <<-EOF
+web: bundle exec puma -C config/puma.rb
+worker: bundle exec sidekiq -q default -q mailers
+redis: redis-server
+EOF
+
+  file "scripts/server", <<-EOF
+#!/bin/sh
+
+heroku local -f Procfile.local
+EOF
 
   git :init
   git add: "."
   git commit: %Q{-m 'Initial commit'}
+
+  run 'heroku create'
+  run 'heroku addons:create heroku-redis:hobby-dev'
+  run "rm README.md"
 end
